5a4f8afd40b92a56aead20b94eae370722322717
spiolynn <hupan_hp@yeah.net>
2018-05-28 19:54:09 +0800

    add
    Signed-off-by: spiolynn <hupan_hp@yeah.net>

--- workshop.py
+++ workshop.py
@@ -1,3 +1,21 @@
 import django
 
-print(django.get_version());
+print(django.get_version())
+
+'''
+# cd C:\Users\wk-hp\AppData\Local\conda\conda\envs\python_dev_36
+# C:\Users\wk-hp\AppData\Local\conda\conda\envs\python_dev_36\python.exe manage.py startapp polls
+C:\Users\wk-hp\AppData\Local\conda\conda\envs\python_dev_36\python.exe manage.py syncdb
+C:\Users\wk-hp\AppData\Local\conda\conda\envs\python_dev_36\python.exe manage.py migrate
+'''
+
+
+'''
+
+### 1 创建一个项目
+django-admin startproject mysite //pycharm自动完成
+
+
+'''
+from news.models import polls.polls
+
